feddcc052acc02b02defebb045f8226e3402c103
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-02-11 17:37:03 +0100

    Aggiunte le immagini dei prodotti e uno sfondo
    Aggiunte le immagini dei prodotti e uno sfondo al main

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -1,15 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach($dati as $dato)
-        @if($dato['tipo'] === "lunga")
-            <h3>{{$dato['titolo']}}</h3>
-        @endif
-        @if($dato['tipo'] === "corta")
-            <h3>{{$dato['titolo']}}</h3>
-        @endif
-        @if($dato['tipo'] === "cortissima")
-            <h3>{{$dato['titolo']}}</h3>
-        @endif
-    @endforeach
+    <main>
+        <h2>LE LUNGHE</h2>
+        <div class="card">
+            @foreach($dati as $dato)
+                @if($dato['tipo'] === "lunga")
+                    <img src="{{ $dato['src'] }}" alt="">
+                @endif
+            @endforeach
+        </div>
+        <h2>LE CORTE</h2>
+        <div class="card">
+            @foreach($dati as $dato)
+                @if($dato['tipo'] === "corta")
+                    <img src="{{ $dato['src'] }}" alt="">
+                @endif
+            @endforeach
+        </div>
+        <h2>LE CORTISSIME</h2>
+        <div class="card">
+            @foreach($dati as $dato)
+                @if($dato['tipo'] === "cortissima")
+                <img src="{{ $dato['src'] }}" alt="">
+                @endif
+            @endforeach
+        </div>
+    </main>
 @endsection
