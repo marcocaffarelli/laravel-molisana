a12f39689e3717a2602df4b19d67a7d84526aaa5
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-02-11 15:44:14 +0100

    Completato l'header
    Inserita l'immagine e creata la navbar contente i tre link

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,4 @@
+@include('layouts.head')
+<body>
+    @include('layouts.header')
+</body>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,10 @@
 <header>
     <div class="logo">
-
+        <img src="{{ asset('/img/logo.png') }}" alt="">
     </div>
     <nav>
-        
+        <a href="/">HOME</a>
+        <a href="prodotti">PRODOTTI</a>
+        <a href="contatti">CONTATTI</a>
     </nav>
 </header>
