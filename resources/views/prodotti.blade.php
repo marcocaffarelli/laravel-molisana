47ab3f2cc42610f70e493231cfe73c55a1af2ad3
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-02-11 18:21:02 +0100

    Aggiunta img e descrizione quando si va in hover
    Aggiunta img e descrizione quando si va in hover

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -3,26 +3,44 @@
 @section('content')
     <main>
         <h2>LE LUNGHE</h2>
-        <div class="card">
+        <div class="cards_container">
             @foreach($dati as $dato)
                 @if($dato['tipo'] === "lunga")
-                    <img src="{{ $dato['src'] }}" alt="">
+                    <div class="card">
+                        <img src="{{ $dato['src'] }}" alt="">
+                        <div class="hover">
+                            <img src="/img/icon.png" alt="">
+                            <p>{{$dato['descrizione']}}</p>
+                        </div>
+                    </div>
                 @endif
             @endforeach
         </div>
         <h2>LE CORTE</h2>
-        <div class="card">
+        <div class="cards_container">
             @foreach($dati as $dato)
                 @if($dato['tipo'] === "corta")
-                    <img src="{{ $dato['src'] }}" alt="">
+                    <div class="card">
+                        <img src="{{ $dato['src'] }}" alt="">
+                        <div class="hover">
+                            <img src="/img/icon.png" alt="">
+                            <p>{{$dato['descrizione']}}</p>
+                        </div>
+                    </div>
                 @endif
             @endforeach
         </div>
         <h2>LE CORTISSIME</h2>
-        <div class="card">
+        <div class="cards_container">
             @foreach($dati as $dato)
                 @if($dato['tipo'] === "cortissima")
-                <img src="{{ $dato['src'] }}" alt="">
+                    <div class="card">
+                        <img src="{{ $dato['src'] }}" alt="">
+                        <div class="hover">
+                            <img src="/img/icon.png" alt="">
+                            <p>{{$dato['descrizione']}}</p>
+                        </div>
+                    </div>
                 @endif
             @endforeach
         </div>
